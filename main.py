752d45b3653dbee9331b2cd50753a0fa3634854b
Gusrifaris Yuda Alhafis <11850112172@students.uin-suska.ac.id>
2021-07-18 22:06:48 +0700

    Initial Commit 🚀

--- main.py
+++ main.py
@@ -22,5 +22,13 @@ async def whoami(ctx) :
 async def clear(ctx, amount=3) :
     await ctx.channel.purge(limit=amount)
 
+@client.event
+async def on_message(message): # Using on_message event reference you dont need to have a prefix
+    guild = message.author.guild
+    if message.content.lower() == "Halo":  # Checks if the WHOLE message is that one word, so not if that one word was part of a message
+        role = discord.utils.get(guild.roles, name="Programmer") # Gets the role
+        if role is not None: # makes sure role exists
+            await message.author.add_roles(role)
+
 
 client.run(token)
