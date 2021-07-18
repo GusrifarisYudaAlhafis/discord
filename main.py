654524b5096516f5faf1c81c474dfc0879b3e1b2
Gusrifaris Yuda Alhafis <11850112172@students.uin-suska.ac.id>
2021-07-19 05:45:56 +0700

    Initial Commit 🚀

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ token = os.getenv("DISCORD_BOT_TOKEN")
 
 @client.event
 async def on_message(message):
-    guild = message.author.guild
-    if message.content.lower() == "Halo":  # Checks if the WHOLE message is that one word, so not if that one word was part of a message
-        role = discord.utils.get(message.guild.roles, name="Programmer") # Gets the role
-        if role is not None: # makes sure role exists
-            await message.author.add_role(role) # Assigns role to the message author
+    user = message.author
+    if message.content.lower() == "Halo":
+        role = discord.utils.get(user.guild.roles, name="Programmer")
+        if role is not None:
+            await user.add_roles(role)
 
 
 
