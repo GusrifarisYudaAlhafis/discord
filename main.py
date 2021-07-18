9f798393a8d8e0b723546c4e6fa6c3c07ff7612e
Gusrifaris Yuda Alhafis <11850112172@students.uin-suska.ac.id>
2021-07-18 22:15:18 +0700

    Initial Commit 🚀

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ async def on_ready() :
     await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
     print("I am online")
 
+@client.event
+async def on_message(message) :
+    guild = message.author.guild
+    if message.content.lower() == "Halo" :
+        role = discord.utils.get(guild.roles, name="Programmer")
+        if role is not None :
+            await message.author.add_roles(role)
+
 @client.command()
 async def ping(ctx) :
     await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
@@ -22,13 +30,7 @@ async def whoami(ctx) :
 async def clear(ctx, amount=3) :
     await ctx.channel.purge(limit=amount)
 
-@client.event
-async def on_message(message) : # Using on_message event reference you dont need to have a prefix
-    guild = message.author.guild
-    if message.content.lower() == "Halo" :  # Checks if the WHOLE message is that one word, so not if that one word was part of a message
-        role = discord.utils.get(guild.roles, name="Programmer") # Gets the role
-        if role is not None : # makes sure role exists
-            await message.author.add_roles(role)
+
 
 
 client.run(token)
