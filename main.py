edf2cfa391bfc1e2a19d0d8b40ddcaa884341371
Gusrifaris Yuda Alhafis <11850112172@students.uin-suska.ac.id>
2021-07-18 22:03:22 +0700

    Initial Commit 🚀

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import discord
+from discord.ext import commands
 import os
 
+client = commands.Bot(command_prefix=".")
 token = os.getenv("DISCORD_BOT_TOKEN")
 
 @client.event
-async def on_message(message):
-    guild = message.author.guild
-    if message.content.startswith('$Halo'):
-        role = discord.utils.get(guild.roles, name="programmer")
-        if role is not None:
-            await message.author.add_roles(role)
+async def on_ready() :
+    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to .help"))
+    print("I am online")
+
+@client.command()
+async def ping(ctx) :
+    await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
+
+@client.command(name="whoami")
+async def whoami(ctx) :
+    await ctx.send(f"You are {ctx.message.author.name}")
+
+@client.command()
+async def clear(ctx, amount=3) :
+    await ctx.channel.purge(limit=amount)
 
 
 client.run(token)
