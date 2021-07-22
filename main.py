8845e6e6dcec94d586773acfa638d72d1a272621
Gusrifaris Yuda Alhafis <11850112172@students.uin-suska.ac.id>
2021-07-22 13:11:49 +0700

    done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+from discord.utils import get
 
 token = os.getenv("DISCORD_BOT_TOKEN")
 
@@ -11,13 +12,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$Halo'):
-        role = discord.utils.get(message.author.server.roles, name='Programmer')
-        await client.add_roles(role)
-        await message.channel.send('Hello!')
-
+    if "Liash" in message.content:
+        await message.author.add_roles(get(message.guild.roles, id=866027113522266192))
 
 client.run(token)
